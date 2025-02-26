c94ef2bfb73749352449c5de3421ea9e390b6ff2
niilun <49104843+niilun@users.noreply.github.com>
2025-02-27 00:14:41 +0200

    add a backtracking example

---
+++ ex/backtracking.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+using namespace std;
+int n;
+
+bool findSuccessorExists(bool sucExists, int x[], int k) {
+    if(x[k] < n) {
+        x[k]++;
+        sucExists = true;
+    }
+    else sucExists = false;
+    return sucExists;
+}
+
+bool checkValid(bool isValid, int x[], int k) {
+    isValid = true;
+    for(int i = 0; i < k; i++) {
+        if(x[i] == x[k]) isValid = false;
+    }
+    return isValid;
+}
+
+bool isSolve(int k) {
+    return k == n;
+}
+
+void printSolve(int arr[], int k) {
+    for(int i = 1; i < n + 1; i++) {
+        cout << arr[i] << ' ';
+    }
+    cout << '\n';
+}
+
+int main()
+{
+    int x[100], k, solveCount = 0;
+    bool sucExists, isValid;
+    x[k] = 0;
+
+    cout << "n: "; cin >> n;
+    cout << "(optional, 0 to ignore) k: "; cin >> k;
+    if(k == 0) k = 1;
+
+    while(k > 0) {
+        sucExists = true; isValid = false;
+        while(sucExists && !isValid) {
+            sucExists = findSuccessorExists(sucExists, x, k);
+            if(sucExists) isValid = checkValid(isValid, x, k);
+        }
+        if(sucExists) {
+            if(isSolve(k)) {
+                printSolve(x, k);
+                solveCount++;
+            }
+            else {
+                k++;
+                x[k] = 0;
+            }
+        }
+        else k--;
+    }
+    cout << solveCount << " solves found.";
+    return 0;
+}
